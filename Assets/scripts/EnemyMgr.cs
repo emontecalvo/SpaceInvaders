d974e08cd49a8354a7d408e9d398f5b2b2f591f4
emontecalvo <emilymontecalvo@gmail.com>
2018-01-23 13:52:39 -0800

    adding enemy

---
+++ Assets/scripts/EnemyMgr.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMgr : MonoBehaviour {
+
+	private static EnemyMgr _inst = null;
+
+	public static EnemyMgr inst {
+		get {
+			if (_inst == null) {
+				_inst = FindObjectOfType<EnemyMgr> ();
+			}
+			return _inst;
+		}
+	}
+
+	public List <Enemy> AllEnemies = new List<Enemy> ();
+
+	public void Register (Enemy enemy) {
+
+		AllEnemies.Add (enemy);
+
+	}
+}
