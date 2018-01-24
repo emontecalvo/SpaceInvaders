43095db55b41c44cdcf2a1ff698a9682e95be9c4
emontecalvo <emilymontecalvo@gmail.com>
2018-01-23 16:14:25 -0800

    adding projectile

--- Assets/scripts/Enemy.cs
+++ Assets/scripts/Enemy.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
+	public Vector3 EnemyPosition;
+
 
 	void Start () {
 		EnemyMgr.inst.Register (this);
+		EnemyPosition = transform.position;
 	}
 
 	void Update () {
 
+	}
+
+
+	public void HandleProjectileImpact() {
+		Debug.Log ("HANDLE IMPACT CALLED");
+		Destroy (gameObject);
+//		CurrentHitPoints -= 1;
+//		if (CurrentHitPoints == 0) {
+//			Destroy (gameObject);
+//		}
 	}
 }
