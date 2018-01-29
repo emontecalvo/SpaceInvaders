f92cfc377bd876e504448041e4209e6081902747
emontecalvo <emilymontecalvo@gmail.com>
2018-01-29 15:28:58 -0800

    updating fire

--- Assets/scripts/Projectile.cs
+++ Assets/scripts/Projectile.cs
@@ -1,34 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Projectile : MonoBehaviour {
 
-	public Enemy Target;
-	public Fighter MyParentFighter;
-
-	public float MySpeed = 1f;
-
-
-	void Start () {
-		Vector3 startPosition = MyParentFighter.transform.position;
-		transform.position = startPosition;
+	void Update () {
+		MovementLogic ();
 	}
 
+	void MovementLogic () {
+		Vector3 speed = Vector3.zero;
 
-	void Update () {
-		Vector3 destination = Target.transform.position;
+		speed.y += 10;
 
-		Vector3 position = transform.position;
-		Vector3 P4 = Target.EnemyPosition;
-		transform.position = P4;
-
-		Debug.DrawLine (position, destination, Color.red);
-
-		if (P4 == destination) {
-			Target.HandleProjectileImpact();
-			Destroy (gameObject);
-		}
+		transform.position = transform.position + speed * Time.deltaTime;
 	}
 
 }
