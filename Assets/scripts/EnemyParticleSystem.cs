bb27c6769d2ce60e9090a3e0815c5a256d159fc5
emontecalvo <emilymontecalvo@gmail.com>
2018-02-06 20:21:55 -0800

    enemy explosion particle

--- Assets/scripts/EnemyParticleSystem.cs
+++ Assets/scripts/EnemyParticleSystem.cs
@@ -4,18 +4,49 @@ using UnityEngine;
 
 public class EnemyParticleSystem : MonoBehaviour {
 
-	float TimeRemaining = 1.0f;
 
-	void Start () {
-		
+//	float TimeRemaining = 1.0f;
+//
+//	void Start () {
+//		
+//	}
+//	
+//
+//	void Update () {
+//		TimeRemaining -= Time.deltaTime;
+//		if (TimeRemaining <= 0) {
+//			Destroy (gameObject);
+//		}
+//		
+//	}
+
+
+
+//	float Lifespan = 1.0f;
+//
+//	void Update() {
+//		Lifespan -= 0.01f;
+//		if (Lifespan <= 0) {
+//			Destroy (gameObject);
+//		}
+//		
+//	}
+
+
+	float FinishTime;
+	float LifeSpan = 1.0f;
+
+	void Start() {
+		float currentTime = Time.time;
+		FinishTime = currentTime + LifeSpan;
+		Debug.Log ("FinishTime:  " + FinishTime);
 	}
-	
 
-	void Update () {
-		TimeRemaining -= Time.deltaTime;
-		if (TimeRemaining <= 0) {
+	void Update() {
+		float currentTime = Time.time;
+		if (currentTime >= FinishTime) {
 			Destroy (gameObject);
 		}
-		
+		Debug.Log ("CurrentTime:  " + currentTime + "   FinishTime:  " + FinishTime);
 	}
 }
